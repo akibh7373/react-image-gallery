ab2ccc74b7d94de82b577f4e98b76390537d7f19
Akib Hossain <akibhossain737@gmail.com>
2023-11-05 13:01:24 -0800

    ADDED: initial commit

---
+++ src/data/data.js
@@ -0,0 +1,13 @@
+export const images = [
+    { id: 1, thumbnail: "/images/image-1.webp" },
+    { id: 2, thumbnail: "/images/image-2.webp" },
+    { id: 3, thumbnail: "/images/image-3.webp" },
+    { id: 4, thumbnail: "/images/image-4.webp" },
+    { id: 5, thumbnail: "/images/image-5.webp" },
+    { id: 6, thumbnail: "/images/image-6.webp" },
+    { id: 7, thumbnail: "/images/image-7.webp" },
+    { id: 8, thumbnail: "/images/image-8.webp" },
+    { id: 9, thumbnail: "/images/image-9.webp" },
+    { id: 10, thumbnail: "/images/image-10.jpeg" },
+    { id: 11, thumbnail: "/images/image-11.jpeg" },
+];
